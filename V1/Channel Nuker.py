ab55bd31bff4e46f30eab805f920ab0d0b9b7322
MinRay <57177408+MinRaych@users.noreply.github.com>
2020-07-31 01:40:29 +0200

    Add files via upload

---
+++ V1/Channel Nuker.py
@@ -0,0 +1,33 @@
+import sys
+import discord
+# - Download discord.py https://pypi.org/project/discord.py/ #
+
+token = "" # <-- Token
+id = "" # <-- Channel's id or User's id
+client = discord.Client()
+
+@client.event
+async def on_connect():
+    channel = client.get_channel(int(id))
+    if channel is not None:
+        async for element in channel.history(limit=999999999999999999999999999999999999999999999999999999999999999999999):
+            try:
+                await element.delete()
+            except:
+                continue
+        sys.exit()
+    else:
+        try:
+            user = client.get_user(int(id))
+            channel = await user.create_dm()
+            async for element in channel.history(limit=999999999999999999999999999999999999999999999999999999999999999999999):
+                try:
+                    await element.delete()
+                except:
+                    continue
+            sys.exit()
+        except:
+            return
+
+
+client.run(token, bot=False)
